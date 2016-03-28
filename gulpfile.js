eca21bd9566f0fe22d014661edd89df3022b9887
Hugo Dias <hugo.victor.dias.teodoro@gmail.com>
2016-03-28 09:30:52 -0300

    Added image optimization suport

--- gulpfile.js
+++ gulpfile.js
@@ -8,8 +8,9 @@ var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
 var coffee = require('gulp-coffee');
 var gutil = require('gulp-util');
+var imageop = require('gulp-image-optimization');
 
-gulp.task('serve', ['sass'], function () {
+gulp.task('serve', ['sass','images','coffee'], function () {
 
     browserSync.init({
         proxy: "http://wptest.dev",
@@ -18,6 +19,11 @@ gulp.task('serve', ['sass'], function () {
 
     gulp.watch("assets/sass/**/*.scss", ['sass']);
     gulp.watch("assets/coffee/**/*.coffee", ['coffee']);
+    gulp.watch([
+            'assets/img/**/*.png',
+            'assets/img/**/*.jpg',
+            'assets/img/**/*.gif',
+            'assets/img/**/*.jpeg'], ['images']);
     gulp.watch("js/*.js").on('change', browserSync.reload);
     gulp.watch("*.php").on('change', browserSync.reload);
     gulp.watch("**/*.php").on('change', browserSync.reload);
@@ -34,6 +40,19 @@ gulp.task('coffee', function () {
     gulp.src('assets/coffee/*.coffee')
         .pipe(coffee({bare: true}).on('error', gutil.log))
         .pipe(gulp.dest('./public/js/'))
+});
+
+gulp.task('images', function (cb) {
+    gulp.src([
+            'assets/img/**/*.png',
+            'assets/img/**/*.jpg',
+            'assets/img/**/*.gif',
+            'assets/img/**/*.jpeg'])
+        .pipe(imageop({
+            optimizationLevel: 5,
+            progressive: true,
+            interlaced: true
+        })).pipe(gulp.dest('public/img')).on('end', cb).on('error', cb);
 });
 
 gulp.task('default', ['serve']);
